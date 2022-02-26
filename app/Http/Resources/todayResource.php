238828957165dfc106386adda9e38c607541a251
oladayo ahmod <oladayoahmod112@gmail.com>
2022-02-26 11:29:11 +0100

    create api for today and upcoming tasks

--- app/Http/Resources/todayResource.php
+++ app/Http/Resources/todayResource.php
@@ -14,6 +14,12 @@ class todayResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+       return [
+           'id'=>$this->id,
+           'completed'=>$this->completed,
+           'approved'=>$this->approved,
+           'title'=>$this->title,
+           'taskId'=>$this->taskId,
+       ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Resources\todayResource;
+use App\Http\Resources\upcomingResource;
+use App\Models\Today;
+use App\Models\Upcoming;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +17,16 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+// upcoming tasks api
+Route::get('/upcoming',function(){
+    $upcoming = Upcoming::all();
+    return upcomingResource::collection($upcoming);
+});
+// today task api
+Route::get('/today',function(){
+    $today = Today::all();
+    return todayResource::collection($today);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
