89bbc2873abe7c9e74b1ad15a810f9a96f13f1d1
oladayo ahmod <oladayoahmod112@gmail.com>
2022-02-28 15:30:45 +0100

    create api for today and upcoming tasks

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Models\Today;
 use App\Models\Upcoming;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\DB;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -27,7 +27,7 @@ Route::get('/upcoming',function(){
 // post upcoming task
 Route::post('/upcoming
 ',function(Request $req){
-    return Today::create([   
+    return Upcoming::create([   
         'taskId'=>$req->taskId,
         'title'=>$req->title,
         'waiting'=>$req->waiting,
@@ -35,11 +35,30 @@ Route::post('/upcoming
         // 'approved'=>$req->approved
     ]);
 });
+// delete upcoming task
+Route::delete('/upcoming{taskId}',function($taskId){
+    DB::table('upcomings')->where('taskId',$taskId)->delete();
+    // Today::destroy($taskId);
+    return 204;
+});
 
 // today task api
+// 
 Route::get('/today',function(){
     $today = Today::all();
     return todayResource::collection($today);
+});
+// post today task
+Route::post('/today',function(Request $req){
+    Today::create([
+        'taskId'=>$req->taskId,
+        'title'=>$req->title,
+        'waiting'=>$req->waiting,
+    ]);
+    // delete today task
+    Route::delete('/today{taskId}',function($taskId){
+        Today::destroy($taskId);
+    });
 });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
