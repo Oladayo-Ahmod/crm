8ff315239ad1b620a47a74b98f9ebc454081b24a
oladayo ahmod <oladayoahmod112@gmail.com>
2022-02-28 18:57:39 +0100

    setting up the UI

--- resources/js/components/Body.vue
+++ resources/js/components/Body.vue
@@ -1,16 +1,25 @@
 <template>
-  <div>
-      <h3>hello world</h3>
+  <div class="body">
+      <Sidebar />
   </div>
 </template>
 
 <script>
-
+import Sidebar from './Sidebar'
 export default {
-    name : "main"
+    name : "main",
+    components : {
+        Sidebar
+    }
 }
 </script>
 
 <style>
-
+.body{
+    background: #15172b;
+    height: 90vh;
+    width: 90%;
+    margin: 0 auto;
+    border-radius: 10px;
+}
 </style>
---
+++ resources/js/components/Sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-5 intro">
+        hello Mr lagabja , how are you doing ?
+      </div>
+    </div>
+    
+      <div class="project-area">
+        <div class="row">
+          <div class="col-md-6">
+
+          </div>
+          <div class="col-md-6">
+
+          </div>
+        </div>
+      </div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name : "Sidebar"
+}
+</script>
+
+<style scoped>
+
+</style>
